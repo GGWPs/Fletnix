267c22572cedb9a53121e242190001676940abc5
GGWP <GGWP@420blaze.it>
2017-12-19 14:08:41 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	afspelen.html
    #	css/filmoverzicht.css

---
+++ login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+</head>
+<body>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lenovo
+ * Date: 19-12-2017
+ * Time: 14:02
+ */
+
+print_r($_GET);
+print_r($_POST);
+?>
+
+</body>
+</html>
