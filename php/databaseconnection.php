20516abd1ed28c95abd846e9d7fc76d9fa00a613
Kingkaene <33867022+Kingkaene@users.noreply.github.com>
2018-01-10 16:58:56 +0100

    Merge branch 'master' of https://github.com/GGWPs/Flexnix-Project
    # Conflicts:
    #	css/basisstijlen.css

--- php/databaseconnection.php
+++ php/databaseconnection.php
@@ -1,21 +1,26 @@
 <?php
-// $hostname = "(local)";
-//$dbname = "FLETNIX_DOCENT";
-//$username = "sa";
-//$pw = "fojmBNHfMyYjHB6boNcZ";
-//$dbh = new PDO ("sqlsrv:Server=$hostname;Database=$dbname;ConnectionPooling=0", "$username", "$pw");
-//$data = $dbh->query("select top 2000 customer_mail_address
-//from Customer");
-//while ($row = $data->fetch()) {
-//    echo "$row firstname $row lastname</br>";
+
+$hostname = "localhost";
+$dbname = "FLETNIX_DOCENT";
+$username = "sa";
+$password = "fojmBNHfMyYjHB6boNcZ"; /* Kaene's wachtwoord Swieber niet stelen  !PAS DIT AAN ALS JE HET WILT TESTEN!
+ (overigens ook de andere waardes hierboven als die anders zijn bij jou*/
+
+try {
+    $dbh = new PDO ("sqlsrv:Server=$hostname;Database=$dbname;ConnectionPooling=0", "$username", "$password");
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo "Er ging iets mis met de database.<br>";   echo "De melding is {$e->getMessage()}<br><br>";
+}
+//@@@@Voorbeelden@@@@@
+//$data = $dbh->query('SELECT TOP 20 * FROM Customer');
+//
+//while ($row = $data->fetch()){
+//    echo "$row[lastname] $row[firstname]</br>";
 //}
 
+//$dbh->query("insert into Person (person_id, lastname, firstname, gender)
+//values (945466, 'Kaene', 'Peters', 'M')");
 
-
-echo '<pre>';
-print_r(PDO::getAvailableDrivers());
-echo '</pre>';
-
-phpinfo();
 
 ?>
