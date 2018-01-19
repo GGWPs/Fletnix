f5b04f69719e7c5411b95befd4fccb27efbddd22
Kingkaene <33867022+Kingkaene@users.noreply.github.com>
2018-01-19 10:40:38 +0100

    aanmeldpagina en abonnement een start naar het php into database proces

--- php/nieuwaccount.php
+++ php/nieuwaccount.php
@@ -11,27 +11,29 @@
 
 include '../php/databaseconnection.php';
 
-print_r($_POST);
 
-$sql = "insert into Customer (customer_mail_address,firstname,lastname,payment_method,payment_card_number,contract_type,subscription_start,subscription_end,user_name,password,country_name)
-         values (:email,:voornaam,:achternaam,null,:rekeningnummer,'basic',getdate(),null,'johnanana123','Pass123','Netherlands')";
+$abonnement = $_POST("abonnement");
+$email = $_POST("email");
+$voornaam = $_POST("voornaam");
+$achternaam = $_POST("achternaam");
+$land = $_POST("land");
+$geboortejaar = $_POST("geboortejaar");
+$betaalMethode = $_POST("betaalMethode");
+$rekeningnummer = $_POST("rekeningnummer");
+$gebruikersnaam = $_POST("gebruikersnaam");
+$wachtwoord = $_POST("wachtwoord");
+$wachtwoord2 = $_POST("wachtwoord2");
+
+$sql = "insert into Customer (customer_mail_address,firstname,lastname,payment_method,payment_card_number,contract_type,subscription_start,subscription_end,user_name,password)
+         values (:email,:voornaam,:achternaam,:betaalmethode,:rekeningnummer,:abonnement,getdate(),null,:gebruikersnaam,:wachtwoord)";
 
 $query = $dbh->prepare($sql);
-$query->execute(
+$query->execute(
     array( ':cijfer' => $cijfer, 	':vak' => $vak	));
 
-//$email = $_POST("email");
-//$voornaam = $_POST("voornaam");
-//$achternaam = $_POST("achternaam");
-//$land = $_POST("land");
-//$geboortejaar = $_POST("geboortejaar");
-//$rekeningnummer = $_POST("rekeningnummer");
-//$gebruikersnaam = $_POST("gebruikersnaam");
-//$wachtwoord = $_POST("wachtwoord");
-//$wachtwoord2 = $_POST("wachtwoord2");
-//
 
 
-$zetnieuwaccountindb = "insert into Customer (customer_mail_address,firstname,lastname,payment_method,payment_card_number,contract_type,subscription_start,subscription_end,user_name,password,country_name)
-                        values ('a@b.nl','asdf','fdasfa','Mastercard',12312312412312,'basic',getdate(),null,'johnanana123','Pass123','Netherlands')";
+
+//$zetnieuwaccountindb = "insert into Customer (customer_mail_address,firstname,lastname,payment_method,payment_card_number,contract_type,subscription_start,subscription_end,user_name,password,country_name)
+//                        values ('a@b.nl','asdf','fdasfa','Mastercard',12312312412312,'basic',getdate(),null,'johnanana123','Pass123','Netherlands')";
 ?>
