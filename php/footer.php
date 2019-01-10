6f74ab04a636883411dcbc64d667c614c84d1d92
Kaene-laptop <JMH.Peters@student.han.nl>
2019-01-10 15:50:29 +0100

    footer code verbeterd

--- php/footer.php
+++ php/footer.php
@@ -1,43 +1,39 @@
-<!--/*-->
-<!-- * Team: Kaene Peters en Ivan Miladinovic-->
-<!-- * Auteur: Kaene en Ivan-->
-<!-- * Versie: 2-->
-<!-- * Datum: 10 januari 2019-->
-<!---->
-<!-- * Aangepast:-->
-<!-- *samenvoeging twee footers bestanden
-<!- *
-<!-*/-->
+<!--
+    Team: Kaene Peters en Ivan Miladinovic
+    Auteur: Kaene en Ivan
+    Versie: 1
+ *  Datum: 10/01/2019
 
-<?php
+     Aangepast:
+    samenvoeging twee footers bestanden, link aangepast
+ *
+-->
 
-echo '<div class="footer"><div class="footer1">';
-if (isset($_SESSION['voornaam'])){
-    echo    '<h2>Menu</h2>
-            <img src="../afbeeldingen/nav.png" width="50px" height="50px" class="navigatie">
+<div class="footer">
+    <div class="footer1">
+        <h2>Menu</h2>
+        <?php
+        if (isset($_SESSION['voornaam'])) {
+            echo '     <img src="../afbeeldingen/nav.png" width="50" height="50" class="navigatie">
                 <a href="../php/filmoverzicht.php?page_id=1">Home</a> 
                 <a href="filmoverzicht.php"> Genre</a> 
                 <a href="overons.php"> Over ons</a>  
-                <a href="../php/voorwaarden.php"> Algemene voorwaarden</a>
-            ';
-} else {
-    echo    '<h2>Menu</h2>
-            <img src="../afbeeldingen/nav.png" width="50px" height="50px" class="navigatie">
+                <a href="../php/voorwaarden.php"> Algemene voorwaarden</a>';
+        } else {
+            echo '  <img src="../afbeeldingen/nav.png" width="50" height="50" class="navigatie">
                 <a href="../php/index.php">Home</a> 
                 <a href="filmoverzicht.php"> Genre</a> 
                 <a href="overons.php"> Over ons</a>  
-                <a href="../php/voorwaarden.php"> Algemene voorwaarden</a>
-            ';
-}
-echo '</div>';
-
-echo    '<div class="footer2"><h2>Contact</h2>
-    <img src="../afbeeldingen/informatie.png" width="50px" height="50px" class="informatie">
-    <p>[T] 0612345678<br>[E] I.Miladinovic@live.nl<br>Fletnix BV<br>Technovium<br>Nijmegen, Nederland</p>
-</div></div>';
-
-echo '<div class="bottom">';
-echo "<p>&copy;", date("Y"), "- Ivan Miladinovic - Kaene Peters - FLETNIX</p>";
-echo '</div>';
-
-?>
+                <a href="../php/voorwaarden.php"> Algemene voorwaarden</a>';
+        }
+        ?>
+    </div>
+    <div class="footer2"><h2> Contact</h2>
+        <img src="../afbeeldingen/informatie.png" alt="informatie.png" width="50" height="50" class="informatie">
+        <p> [T] 0612345678 <br>[E] I . Miladinovic@live . nl <br>Fletnix BV <br>Technovium <br>Nijmegen,
+            Nederland </p>
+    </div>
+</div>
+<div class="bottom">';
+    <p>&copy;", <?php echo 'date("Y"),' ?> "- Ivan Miladinovic - Kaene Peters - FLETNIX</p>";
+</div>;
