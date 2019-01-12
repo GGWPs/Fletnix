05da5f00a9c9fbbf8526e6cf5dfd8325dc95ee9a
I.Miladinovic@student.han.nl <sw8bW5Shc2uhnGxylW8i>
2019-01-12 01:09:22 +0100

    password hash toegevoegd, toegevoegd dat als gebruiker met niet gehashte ww inlogt, automatisch verandert naar hash

--- php/login.php
+++ php/login.php
@@ -6,7 +6,8 @@
 <!---->
 <!-- * Aangepast:-->
 <!-- * - aangepast-->
-<!-- * - header toegevoegd die terug gaat en gebruiker laat weten dat gegevens onjuist zijn, password hash toegevoegd
+<!-- * - header toegevoegd die terug gaat en gebruiker laat weten dat gegevens onjuist zijn, password hash toegevoegd,
+         toegevoegd dat als gebruiker met niet gehashte ww inlogt, automatisch verandert naar hash
 </-->
 
 <?php
@@ -18,9 +19,20 @@ try {
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
     $password_from_db = $result["password"];
     if(!isset($password_from_db)){
-        echo "password from db";
+        echo "werkt ni";
     } else if(!password_verify($_POST["wachtwoord"],$password_from_db)){ //If the result is empty then there are no users with the submitted username+password
-        echo "wwww";
+        if($_POST["wachtwoord"] == $password_from_db){
+            $hash=password_hash($password_from_db, PASSWORD_DEFAULT);
+            $stmt = verbindDatabase()->prepare("UPDATE Customer SET password = :value1 WHERE user_name = :value2");
+            $stmt->execute(array(":value1" => $hash,":value2" => $_POST["gebruikersnaam"]));
+            $_SESSION['voornaam'] = $result["firstname"];
+            $_SESSION['achternaam'] = $result["lastname"];
+            setlocale(LC_ALL, 'nl_NL') or setlocale(LC_ALL, 'nld_NLD');
+            $_SESSION['logintijd'] = date("H:i") ;
+            header("Location: ../php/filmoverzicht.php?page_id=1");
+            die();
+        }
+        header("location:aanmeldpagina.php?msg=fout");
     } else {
         $_SESSION['voornaam'] = $result["firstname"];
         $_SESSION['achternaam'] = $result["lastname"];
